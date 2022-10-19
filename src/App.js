f0b6a5e456dfbe2fc7ccf8b3fb98251eb8b802c3
akashahmmed303 <ahmedakash336@gmail.com>
2022-10-20 04:48:01 +0600

    added sign out btn

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import app from './firebase/firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
 import './App.css';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
+
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then(result => {
         const user = result.user;
+        setUser(user);
         console.log(user);
       })
       .catch(error => {
@@ -17,9 +26,33 @@ function App() {
       });
   };
 
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch(() => {
+        setUser({});
+      });
+  };
+
   return (
     <div className='App'>
-      <button onClick={handleGoogleSignIn}>Google sign in</button>
+      {/* condition ? true: false */}
+
+      {user.email ? (
+        <button onClick={handleGoogleSignIn}>Google sign in</button>
+      ) : (
+        <button onClick={handleSignOut}>Sign Out</button>
+      )}
+
+      {user.email && (
+        <div>
+          <h3>User name: {user.displayName}</h3>
+          <p>Email address: {user.email}</p>
+          <img src={user.photoURL} alt='' />
+        </div>
+      )}
     </div>
   );
 }
