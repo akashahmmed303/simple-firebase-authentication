1666f8afae9e4e424d6537bdd1e00127269aa07f
akashahmmed303 <ahmedakash336@gmail.com>
2022-10-20 03:16:35 +0600

    google sign in btn added

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBfboWfWxpAVa1TjEDsHaiPa3b4WgnqWsc',
+  authDomain: 'simple-firebase-authenti-45b83.firebaseapp.com',
+  projectId: 'simple-firebase-authenti-45b83',
+  storageBucket: 'simple-firebase-authenti-45b83.appspot.com',
+  messagingSenderId: '611932378460',
+  appId: '1:611932378460:web:ee6acde6bd78ffd1f59d99',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
